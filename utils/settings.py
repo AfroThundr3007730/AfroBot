9004e737d2e1005c8b1539b3a7c29efad945977a
Eddie James Carswell II <eddiecarswell13@yahoo.com>
2019-09-29 14:28:58 -0700

    utils/settings.py: convert Settings to be a wrapper for Box

--- utils/settings.py
+++ utils/settings.py
@@ -1,7 +1,7 @@
-import io
 import os
 
-from box import Box
+from box import Box, BoxList
+
 
 class Settings(object):
     _config_location = 'config.json'
@@ -16,6 +16,26 @@ class Settings(object):
     def __enter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_value, tb):
+    def __exit__(self, type, value, traceback):
         self.__dict__.to_json(filename=self._config_location,
                               sort_keys=True, indent=4)
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        if type(value) is list:
+            return setattr(self, key, BoxList(value))
+        elif type(value) is dict:
+            return setattr(self, key, Box(value))
+        else:
+            return setattr(self, key, value)
+
+    def __delitem__(self, key):
+        return delattr(self, key)
+
+    def __repr__(self):
+        return repr(self.__dict__)
+
+    def save(self):
+        return self.__exit__(None, None, None)
